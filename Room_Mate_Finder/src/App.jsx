fbe9bb2a4db8de659eef8819561d9f84c9dfcb06
GauravKumarDhiman <139962975+AARAVGAURAV@users.noreply.github.com>
2025-03-03 15:58:56 +0000

    new changes-2

--- Room_Mate_Finder/src/App.jsx
+++ Room_Mate_Finder/src/App.jsx
@@ -6,27 +6,31 @@ function App() {
 
   return (
     <>
+    {/* start Header Nav bar include wrapper contain all element */}
     <nav>
-      <div className="Wrapper">
+      <Wrapper>
         
           <ul>
+
           <li>Home</li>
           <li>About-US</li>
           <li>Contact-us</li>
           <li>Sign-Up</li>
           <li>Login</li>
           <li>Profile</li>
+          
           </ul>
         
-      </div>
-      </nav>
+      </Wrapper>
+</nav>
+      {/* end Header Nav bar include wrapper contain all element */}
     </>
   )
 }
 
 export default App
 
-// const example = styled.div`
+const Wrapper = styled.div`
 
-// `;
+`;
 
