d2ba9dd118f9d0023b767ee2b6902a60302e1b46
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-05-25 01:39:10 +0530

    db models

---
+++ server/model/user.model.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: String,
+  isOwner: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+export const User = model("user", UserSchema);
